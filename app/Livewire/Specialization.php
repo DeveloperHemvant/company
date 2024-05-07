430c1efd589504967e6f4363dbdce21e3f0f2026
DeveloperHemvant <developer.hemvant@gmail.com>
2024-05-07 19:01:35 +0530

    specialization working is going on with updating the specification error

--- app/Livewire/Specialization.php
+++ app/Livewire/Specialization.php
@@ -7,36 +7,27 @@ use Livewire\Component;
 use App\Models\Cousre;
 use App\Models\specializations;
 
-
-
-
-
 class Specialization extends Component
 {
     public $showAddForm = false;
-
     public $showEditForm = false;
     public $courses;
     public $course_name;
     public $specialization_name;
     public $u_specialization_name;
     public $special_data;
+
     public function mount()
     {
         $this->courses = Cousre::all();
-        $this->special_data = specializations::with('cousre')->get();
-
+        $this->refreshData();
     }
 
     public function toggleAddForm()
     {
         $this->showAddForm = !$this->showAddForm;
-        $this->reset(['specialization_name', 'course_name']);
         $this->showEditForm = false;
-
     }
-
-
 
     public function save()
     {
@@ -50,55 +41,53 @@ class Specialization extends Component
             'course_name' => 'required'
         ]);
 
-        $specialization = new specializations;
-        $specialization->specialization_name = $this->specialization_name;
-        $specialization->course_id = $this->course_name;
-        $specialization->save();
+        specializations::create([
+            'specialization_name' => $this->specialization_name,
+            'course_id' => $this->course_name
+        ]);
 
-        // $this->reset(['specialization_name', 'course_name']);
-        $this->specialization_name = '';
-        $this->course_name = '';
-        // $this->emit('specializationAdded'); 
-        $this->showAddForm = false;
+        $this->refreshData();
+        $this->toggleAddForm();
     }
     public $s_id;
     public $c_id;
-    public $u_course_name;
     public function edit($id)
     {
         $this->showEditForm = true;
+        $specialization = specializations::find($id);
         $this->s_id = $id;
-        $special_data = specializations::find($id);
-        // dd($u_specialization_name);
-        $this->u_specialization_name = $special_data->specialization_name;
-
-        $this->c_id = $special_data->course_id;
+        $this->u_specialization_name = $specialization->specialization_name;
+        $this->u_course_name = $specialization->course_id;
+        
     }
+
     public function update()
     {
-        // dd('hello');
-        $specializationId = $this->s_id;
-        $data = specializations::find($specializationId);
-        if ($data->id != $specializationId) {
-            $validatedData = $this->validate([
-                'u_specialization_name' => [
-                    'required',
-                    Rule::unique('specializations', 'specialization_name')->where(function ($query) {
-                        return $query->where('course_id', $this->c_id);
-                    })
-                ],
+        $validatedData = $this->validate([
+            'u_specialization_name' => [
+                'required',
+                Rule::unique('specializations', 'specialization_name')->where(function ($query) {
+                    return $query->where('course_id', $this->u_course_name);
+                })->ignore($this->s_id),
+            ],
+            'u_course_name' => 'required'
+        ]);
 
-                'u_course_name' => 'required:specializations,course_name'
-            ]);
-        }
-        $data->where('id', $specializationId)->update([
+        $specialization = specializations::find($this->s_id);
+        $specialization->update([
             'specialization_name' => $this->u_specialization_name,
             'course_id' => $this->u_course_name
         ]);
+
+        $this->refreshData();
         $this->showEditForm = false;
-
-
     }
+
+    public function refreshData()
+    {
+        $this->special_data = specializations::with('cousre')->get();
+    }
+
     public function render()
     {
         return view('livewire.specialization');
